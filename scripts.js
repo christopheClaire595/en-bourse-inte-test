7939e6574a81e5a8201abecc782e7394f99e0dcd
Christophe <claire0christophe@gmail.com>
2020-10-05 17:20:09 +0200

    enbourse | test v2

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-$('button').each(function(){
+$('button:not(.close)').each(function(){
 	$(this).on('click',function(){
 		$(this).html('<i class="las la-spinner"></i>').attr("disabled", false).css('cursor','not-allowed');
 		$('#exampleModal').modal('toggle')
