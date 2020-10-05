c54e4b2140518096d4d91d643772fca649c95bd7
Christophe <claire0christophe@gmail.com>
2020-10-05 17:28:23 +0200

    enbourse | test v2

--- scripts.js
+++ scripts.js
@@ -1,6 +1,11 @@
 $('button:not(.close)').each(function(){
 	$(this).on('click',function(){
-		$(this).html('<i class="las la-spinner"></i>').attr("disabled", false).css('cursor','not-allowed');
+		$(this).html('<i class="las la-spinner"></i>').attr("disabled", true).css('cursor','not-allowed');
 		$('#exampleModal').modal('toggle')
 	})
 })
+
+
+$('#exampleModal .close').on('click',function(){
+	$('button:not(.close)').attr("disabled", false).css('cursor','pointer').text('Être informer de l’ouverture')
+});
